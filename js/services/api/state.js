b98b622a98fd1253c50e22e05a9cc1173404d97b
skillzTheProgrammer <47523568+skillzTheProgrammer@users.noreply.github.com>
2020-09-26 10:54:42 +0100

    made base url https

--- js/services/api/state.js
+++ js/services/api/state.js
@@ -1,4 +1,4 @@
-const baseUrl = "http://5f6ca1ee34d1ef0016d58592.mockapi.io/api/v2";
+const baseUrl = "https://5f6ca1ee34d1ef0016d58592.mockapi.io/api/v2";
 const loader = document.getElementById("loader");
 const pagination = document.getElementById("pagination");
 // const editCommentBtn = document.getElementById("editComment");
